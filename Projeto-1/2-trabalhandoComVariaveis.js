7734509ff5f4a0b595f2cca434a0632c5d5a63ff
Larissa Nogueira <larissanogueira033@gmail.com>
2022-05-10 18:07:07 -0300

    praticando com variáveis

---
+++ Projeto-1/2-trabalhandoComVariaveis.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 18; //declaração de variável
+const nome = "Larissa";
+// let ano = 2022; //declaração alternativa
+
+console.log = 29;
---
+++ Projeto-1/5-atribuicaoVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 18; //declaração de variável
+let  nome = "Larissa"; // let pode variar 
+const sobrenome = "Nogueira";
+
+// console.log(nome + "" + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+// let ano = 2022; //declaração alternativa
+
