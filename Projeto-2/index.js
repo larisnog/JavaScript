8d931a7939dbaf51d2d56cf9dcd4f677324a4a25
Larissa Nogueira <larissanogueira033@gmail.com>
2022-05-18 11:46:15 -0300

    comportamentos de classes; atributos privados; métodos com retorno

--- Projeto-2/index.js
+++ Projeto-2/index.js
@@ -1,28 +1,44 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
-    saldo;
-    rg;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0 ){
+            return;
+        }
+
+        this._saldo += valor;
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Larissa";
 cliente1.cpf = 11122233309;
-cliente1.agencia = "1001";
-cliente1.saldo = 0;
-cliente1.rg = 123456789;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = "1001";
-cliente2.saldo = 0;
 
-const Cliente3Nome = "Alice";
-const Cliente3CPF = 88822233309;
-const Cliente3Agencia = "1001";
-const Cliente3Saldo = 0;
+const contaCorrenteLarissa = new ContaCorrente();
+contaCorrenteLarissa.agencia= 1001;
 
-console.log(cliente1, cliente2);
+contaCorrenteLarissa.depositar(-100);
+contaCorrenteLarissa.depositar(100);
+contaCorrenteLarissa.depositar(100);
+
+const valorSacado = contaCorrenteLarissa.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteLarissa);
