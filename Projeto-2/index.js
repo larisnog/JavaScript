1872a2045ec04f55c7b718704099377083a3c538
Larissa Nogueira <larissanogueira033@gmail.com>
2022-05-19 15:54:10 -0300

    construtores

--- Projeto-2/index.js
+++ Projeto-2/index.js
@@ -1,25 +1,14 @@
 import{Cliente} from "./Cliente.js" 
 import{ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Larissa";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Larissa", 1112223330);
+const cliente2 = new Cliente("Alice", 88822233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
-const contaCorrenteLarissa = new ContaCorrente();
-contaCorrenteLarissa.agencia= 1001;
-contaCorrenteLarissa.cliente = cliente1;
+const contaCorrenteLarissa = new ContaCorrente(1001, cliente1);
 contaCorrenteLarissa.depositar(500);
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente1);
 
 let valor = 200;
 contaCorrenteLarissa.tranferir(valor, conta2);
 
-console.log("valor:", valor);
-console.log(conta2.cliente);
+console.log(ContaCorrente.numeroDeContas);
