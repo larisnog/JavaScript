27d8ba4a6e7aa71b7631fb6f419427224a53047d
Larissa Nogueira <larissanogueira033@gmail.com>
2022-05-24 16:31:03 -0300

    mexendo com herança, super e sobreescrita

--- Projeto-2/index.js
+++ Projeto-2/index.js
@@ -1,16 +1,16 @@
 import{Cliente} from "./Cliente.js" 
-import { Conta } from "./Conta.js";
 import{ContaCorrente} from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Larissa", 1112223330);
 const cliente2 = new Cliente("Alice", 88822233309);
 
-const contaCorrenteLarissa = new Conta(0, cliente1, 1001);
+const contaCorrenteLarissa = new ContaCorrente(cliente1, 1001);
 contaCorrenteLarissa.depositar(500);
 contaCorrenteLarissa.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteLarissa);
